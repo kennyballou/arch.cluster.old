3b3e9d026caa376655084165636fe962a603d7f9
kennyballou <kballou@onyx.boisestate.edu>
2012-12-13 09:24:19 -0700

    Update default node classes

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,15 +1,10 @@
 import "headless"
 import "desktop"
-import "cuda"
 
 node "headless" {
     include headless
 }
 
-node "cuda" {
-    include headless, cuda
-}
-
 node "desktop" {
-    include desktop, cuda
+    include desktop
 }
